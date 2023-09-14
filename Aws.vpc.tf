08d877504ffa1e579891cd233c55f751060dea44
Kasireddy-swathi <swathikasireddy250@gmail.com>
2023-09-14 08:46:24 +0530

    create VPC

--- Aws.vpc.tf
+++ Aws.vpc.tf
@@ -1,5 +1,5 @@
 #create VPC
-resource "aws_vpc" "VPC-01" {
+resource "aws_vpc" "VPC-02" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
--- aws delete vpc.tf
+++
@@ -1,4 +0,0 @@
-#delete vpc
-resource "aws_vpc" "VPC-01" {
-  force_destroy = true
-}
